40ccbe6c1eaea25ff04164e0fc0ec4840da28903
miruuww <you@example.com>
2023-08-31 16:45:26 +0700

    auth complete

--- app.js
+++ app.js
@@ -5,9 +5,6 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv/config');
-const authJwt = require('./helpers/jwt');
-;
-// const errorHandler = require('./helpers/error-handler');
 
 app.use(cors());
 app.options('*', cors());
@@ -15,29 +12,12 @@ app.options('*', cors());
 // deklarasi middleware
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
-// app.use(authJwt());
-// app.use(errorHandler());
-app.use('/public/upload', express.static(__dirname + '/public/uploads'));
 app.use((err, req, res, next) => {
     if (err) {
         res.status(500).json({message: err})
     }
 })
 
-// routes 
-const categoriesRoutes = require('./routes/categories.router');
-const productRoutes = require('./routes/products.router')
-const userRoutes = require('./routes/users.router');
-const orderRoutes = require('./routes/orders.router');
-// deklarasi dari file .env
-const api = process.env.API_URL;
-// deklarasi router express
-app.use(`${api}/categories`, categoriesRoutes);
-app.use(`${api}/products`, productRoutes);
-app.use(`${api}/users`, userRoutes);
-app.use(`${api}/orders`, orderRoutes);
-// deklarasi mongoose untuk koneksi ke db compass
-mongoose.set("strictQuery", false);
 mongoose.connect(process.env.CONNECTION_STRING, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -50,10 +30,21 @@ mongoose.connect(process.env.CONNECTION_STRING, {
         console.log(err);
     })
 // deklarasi port di 8080
-const port = 8000;
+const port = 7000;
 app.listen(port, ()=> {
     console.log(`server is running http://localhost:${port}`);
 })
 
-
-
+// routes 
+// const categoriesRoutes = require('./routes/categories.router');
+// const productRoutes = require('./routes/products.router')
+const userRoutes = require('./routes/users.router');
+// const orderRoutes = require('./routes/orders.router');
+// deklarasi dari file .env
+// deklarasi router express
+// app.use(`${api}/categories`, categoriesRoutes);
+// app.use(`${api}/products`, productRoutes);
+app.use(userRoutes);
+// app.use(`${api}/orders`, orderRoutes);
+// deklarasi mongoose untuk koneksi ke db compass
+mongoose.set("strictQuery", false);
--- routes/users.router.js
+++ routes/users.router.js
@@ -1,162 +1,40 @@
-const { User } = require('../models/user.model');
 const express = require('express');
+
 const router = express.Router();
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const {
+    createUser,
+    userSignIn,
+    uploadProfile,
+    signOut,
+} = require('../controller/user.controller');
+const { isAuth } = require('../middleware/auth');
+const {
+  validateUserSignUp,
+    userVlidation,
+    validateUserSignIn,
+} = require('../middleware/validation/user.validation');
 
-router.get('/', async (req, res) => {
-    const userList = await User.find()
-    // .select('-passwordHash');
+const multer = require('multer');
 
-    if (!userList) {
-        res.status(500).json({ success: false });
-    }
-    res.send(userList);
-})
+const storage = multer.diskStorage({});
 
-router.get('/:id', async (req, res) => {
-    const user = await User.findById(req.params.id)
-    // .select('-passwordHash');
-
-    if (!user) {
-        res.status(500).json({ message: "pengguna dengan ID yang diberikan tidak ditemukan!" })
-    }
-
-    res.status(200).send(user);
-})
-
-router.post('/', async (req, res) => {
-    let user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        passwordHash: bcrypt.hashSync(req.body.password, 10),
-        phone: req.body.phone,
-        isAdmin: req.body.isAdmin,
-        apartement: req.body.apartement,
-        zip: req.body.zip,
-        city: req.body.city,
-        country: req.body.country
-    })
-    user = await user.save();
-
-    if (!user)
-        return res.status(404).send('pengguna tidak dapat dibuat!')
-
-    res.send(user);
-})
-
-router.put('/:id', async (req, res) => {
-    const userExist = await User.findById(req.params.id);
-    let newPassword
-    if (req.body.password) {
-        newPassword = bcrypt.hashSync(req.body.password, 10);
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype.startsWith('image')) {
+      cb(null, true);``
     } else {
-        newPassword = userExist.passwordHash;
+      cb('invalid image file!', false);
     }
-
-    const user = await User.findByIdAndUpdate(
-        req.params.id,
-        {
-            name: req.body.name,
-            email: req.body.email,
-            passwordHash: newPassword,
-            phone: req.body.phone,
-            isAdmin: req.body.isAdmin,
-            apartement: req.body.apartement,
-            zip: req.body.zip,
-            city: req.body.city,
-            country: req.body.country
-        },
-        { new: true }
-    )
-
-    if (!user)
-        return res.status(400).send('pengguna tidak dapat diperbarui!')
-
-    res.send(user);
-})
-
-// Register User
-router.post('/register', async (req, res) => {
-    const { name, email, password, phone, street, apartment, zip, city, country } = req.body;
-
-    // Memeriksa apakah pengguna sudah ada
-    let user = await User.findOne({ email });
-    if (user) {
-        return res.status(400).send('Email sudah terdaftar!');
-    }
-
-    // Hash password
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-
-    // Membuat user baru
-    user = new User({
-        name,
-        email,
-        passwordHash: hashedPassword,
-        phone,
-        street,
-        apartment,
-        zip,
-        city,
-        country,
-    });
-
-    // Menyimpan user ke database
-    user = await user.save();
-
-    if (!user) {
-        return res.status(500).send('Registrasi gagal! Silakan coba lagi.');
-    }
-
-    res.send({
-        userId: user.id,
-        name: user.name,
-        email: user.email,
-    });
-});
-
-// Login user
-router.post('/login', async (req, res) => {
-    const { email, password } = req.body;
-
-    // Memeriksa apakah pengguna sudah ada
-    const user = await User.findOne({ email });
-    if (!user) {
-        return res.status(400).send('Email atau password salah!');
-    }
-
-    // Memvalidasi password
-    const validPassword = await bcrypt.compare(password, user.passwordHash);
-    if (!validPassword) {
-        return res.status(400).send('Email atau password salah!');
-    }
-
-    res.send('Login berhasil!');
-});
-
-router.delete('/:id', (req, res) => {
-    User.findByIdAndRemove(req.params.id).then(user => {
-        if (user) {
-            return res.status(200).json({ success: true, message: 'user berhasil dihapus!' })
-        } else {
-            return res.status(404).json({ success: true, message: 'user tidak ada!' })
-        }
-    }).catch(err => {
-        return res.status(400).json({ success: false, error: err })
-    })
-})
-
-router.get('/get/count', async (req, res) => {
-    const userCount = await User.countDocuments();
-
-    if (!userCount) {
-        res.status(500).json({ success: false })
-    }
-    res.send({
-        userCount: userCount
-    })
-})
-
-module.exports = router;
+  };
+  const uploads = multer({ storage, fileFilter });
+  
+  router.post('/create-user', validateUserSignUp, userVlidation, createUser);
+  router.post('/sign-in', validateUserSignIn, userVlidation, userSignIn);
+  router.post('/sign-out', isAuth, signOut);
+  router.post(
+    '/upload-profile',
+    isAuth,
+    uploads.single('profile'),
+    uploadProfile
+  );
+  
+  module.exports = router;
